977646eea4c4e57b18e4b48ccee2022f662aef9c
uendercarlos <44954880+uendercarlos@users.noreply.github.com>
2020-04-06 11:46:01 -0300

    Update conecta.php

--- conecta.php
+++ conecta.php
@@ -1,9 +1,9 @@
 <?php
 
 $servidor = "localhost";
-$usuario = "77759";
-$senha = "Catitu.835835";
-$dbname = "77759";
+$usuario = "seu usuario";
+$senha = "sua senha";
+$dbname = "seu banco";
 
 //criar conexao
 $con = mysqli_connect($servidor, $usuario, $senha, $dbname)or die( 'Erro de conexao ao BD: '.mysqli_error( $conexao ) );
